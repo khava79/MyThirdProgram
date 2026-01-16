a7160ad466f88008764fba11f4471cd81348f964
khava79 <havatsechoeva011@gmail.com>
2026-01-16 23:25:45 +0300

    Remove commented ssn initialization error
    Removed commented-out erroneous initialization of ssn.

--- src/main/java/practice3/Person.java
+++ src/main/java/practice3/Person.java
@@ -4,6 +4,8 @@ public class Person {
     private String firstName;
     private String lastName;
     private final String ssn;
+    
+    // private final String ssn = "SSn"; - ошибка!
 
     Person(String firstName, String lastName, String ssn) {
         this.firstName = firstName;
