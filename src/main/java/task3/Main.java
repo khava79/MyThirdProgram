09e5d32a36e5f71b8675b5250738fc4fd7cd0ca5
khava79 <havatsechoeva011@gmail.com>
2026-01-21 01:30:49 +0300

    Update Main.java

--- src/main/java/task3/Main.java
+++ src/main/java/task3/Main.java
@@ -3,16 +3,13 @@ package task3;
 public class Main {
     public static void main(String[] args) {
         Menu menu = new Menu();
-
-        Dish steak = new HotDish("Стейк", 75.0);
-        menu.setDish(steak);
+        
+        addAndShowDish(menu, new HotDish("Стейк", 75.0));
+        addAndShowDish(menu, new Drink("Яблочный сок", 350));
+    }
+    
+    private static void addAndShowDish(Menu menu, Dish dish) {
+        menu.setDish(dish);
         menu.printDish();
-
-
-
-        Dish juice = new Drink("Яблочный сок", 350);
-        menu.setDish(juice);
-        menu.printDish();
-
     }
 }
