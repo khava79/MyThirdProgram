9cb6d21337c185c93707ab648fb7e8dbe0ab38c0
khava79 <havatsechoeva011@gmail.com>
2026-01-21 01:22:19 +0300

    Update RollerCoaster.java

--- src/main/java/task7/RollerCoaster.java
+++ src/main/java/task7/RollerCoaster.java
@@ -3,11 +3,11 @@ package task7;
 public class RollerCoaster implements Attraction {
     @Override
     public void info() {
-        System.out.println("Амереканские горки: острые ощущения.");
+        System.out.println("Американские горки: острые ощущения.");
     }
 
     @Override
     public void maintain() {
-        System.out.println("Амереканские горки: требуется проверка безопасности.");
+        System.out.println("Американские горки: требуется проверка безопасности.");
     }
 }
