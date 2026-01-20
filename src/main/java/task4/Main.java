955486059f316aaa1bc18836cd9889e03a95477f
khava79 <havatsechoeva011@gmail.com>
2026-01-21 01:32:00 +0300

    Update Main.java

--- src/main/java/task4/Main.java
+++ src/main/java/task4/Main.java
@@ -3,13 +3,13 @@ package task4;
 public class Main {
     public static void main(String[] args) {
         Aquarium aquarium = new Aquarium();
-
-        SeaCreature shark = new Shark();
-        aquarium.setCreature(shark);
-        aquarium.showMovement();
-
-        SeaCreature starfish = new StarFish();
-        aquarium.setCreature(starfish);
+        
+        addAndShowMovement(aquarium, new Shark());
+        addAndShowMovement(aquarium, new StarFish());
+    }
+    
+    private static void addAndShowMovement(Aquarium aquarium, SeaCreature creature) {
+        aquarium.setCreature(creature);
         aquarium.showMovement();
     }
 }
