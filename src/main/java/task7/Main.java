3a511b47025fc8bfe9206cf5b39d3272738cdddd
khava79 <havatsechoeva011@gmail.com>
2026-01-21 01:33:58 +0300

    Update Main.java

--- src/main/java/task7/Main.java
+++ src/main/java/task7/Main.java
@@ -1,17 +1,15 @@
 package task7;
 
-
 public class Main {
     public static void main(String[] args) {
         AmusementPark park = new AmusementPark();
-
-        Attraction rc = new RollerCoaster();
-        park.setAttraction(rc);
-        park.operateAttraction();
-
-
-        Attraction carousel = new Carousel();
-        park.setAttraction(carousel);
+        
+        addAndOperate(park, new RollerCoaster());
+        addAndOperate(park, new Carousel());
+    }
+    
+    private static void addAndOperate(AmusementPark park, Attraction attraction) {
+        park.setAttraction(attraction);
         park.operateAttraction();
     }
 }
