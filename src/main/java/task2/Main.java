b8033ccec72955416fd8e923031564c496884332
khava79 <havatsechoeva011@gmail.com>
2026-01-21 01:35:24 +0300

    Update Main.java

--- src/main/java/task2/Main.java
+++ src/main/java/task2/Main.java
@@ -3,18 +3,14 @@ package task2;
 public class Main {
     public static void main(String[] args) {
         PetManager manager = new PetManager();
-
-        Pet dog = new Dog();
-        manager.setPet(dog);
+        
+        addAndHandle(manager, new Dog());
+        addAndHandle(manager, new Cat());
+    }
+    
+    private static void addAndHandle(PetManager manager, Pet pet) {
+        manager.setPet(pet);
         manager.handlePet();
         System.out.println();
-
-
-        Pet cat = new Cat();
-        manager.setPet(cat);
-        manager.handlePet();
-        System.out.println();
-
-
     }
 }
