cab23b069f362bf1bb385b77aee598d47416bad3
khava79 <havatsechoeva011@gmail.com>
2026-01-21 01:33:27 +0300

    Update Main.java

--- src/main/java/task6/Main.java
+++ src/main/java/task6/Main.java
@@ -3,14 +3,13 @@ package task6;
 public class Main {
     public static void main(String[] args) {
         BotanicalGarden garden = new BotanicalGarden();
-
-        Plant orchid = new Orchid();
-        garden.setPlant(orchid);
+        
+        addAndMaintain(garden, new Orchid());
+        addAndMaintain(garden, new Cactus());
+    }
+    
+    private static void addAndMaintain(BotanicalGarden garden, Plant plant) {
+        garden.setPlant(plant);
         garden.maintainPlant();
-
-        Plant cactus = new Cactus();
-        garden.setPlant(cactus);
-        garden.maintainPlant();
-
     }
 }
