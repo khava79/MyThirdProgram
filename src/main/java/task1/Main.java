edb1a64358bd1be336401f64752481c642a29216
khava79 <havatsechoeva011@gmail.com>
2026-01-21 01:34:54 +0300

    Update Main.java

--- src/main/java/task1/Main.java
+++ src/main/java/task1/Main.java
@@ -3,18 +3,15 @@ package task1;
 public class Main {
     public static void main(String[] args) {
         Zoo zoo = new Zoo();
-
-        Animal elephant = new Elephant();
-        zoo.setAnimal(elephant);
-        System.out.println("Добавили слона");
+        
+        addAndShowAnimal(zoo, new Elephant(), "слона");
+        addAndShowAnimal(zoo, new Bird(), "птицу");
+    }
+    
+    private static void addAndShowAnimal(Zoo zoo, Animal animal, String animalName) {
+        zoo.setAnimal(animal);
+        System.out.println("Добавили " + animalName);
         zoo.showAnimal();
         System.out.println();
-
-        Animal bird = new Bird();
-        zoo.setAnimal(bird);
-        System.out.println("Добавили птицу");
-        zoo.showAnimal();
-        System.out.println();
-
     }
 }
